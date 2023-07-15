cecae1297ad6b72dfab73657af4b38819a977d2a
Santiago_Puchetti <129624449+SanGusTti@users.noreply.github.com>
2023-07-15 12:52:13 -0300

    ControlDeTemperatura.c

---
+++ ControlDeTemperatura.c
@@ -0,0 +1,14 @@
+//Aquí se detallará la funcion de control de temperatura
+
+# include <stdlib.h>
+# include <stdio.h>
+
+int TempCtrl () 
+{
+  struct 
+  {
+  char SetTempInicio[4], TempZona[4];
+  
+  } Struct_Temps;
+  
+}
