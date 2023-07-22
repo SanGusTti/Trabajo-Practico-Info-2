6af0bca0c1f6259dd95092e0f17c8d308d502818
Santiago_Puchetti <129624449+SanGusTti@users.noreply.github.com>
2023-07-22 08:46:23 -0300

    V2ControlDeTemperatura.c

--- ControlDeTemperatura.c
+++ ControlDeTemperatura.c
@@ -1,14 +1,58 @@
-//Aquí se detallará la funcion de control de temperatura
+//Aqui se define la funcion de control de temperatura en el horno y el calentamiento del mismo//
 
-# include <stdlib.h>
-# include <stdio.h>
+	//Inicio: Preguntar setteos de temperaturas iniciales (Set_TempZona[4]) *Ver de ampliar a cargar un archivo de base de datos seteada 
+	//Toma de mediciones con termocuplas 
+	//Barrote en la entrada? Sí, abrir puerta y avanzar cadena.
+	//Avanzar barrote lentamente por zonas, hasta que alcance la temperatura seteada por zona
+	//Abrir puerta, avanzar cadena, cae el barrote, cerrar puerta.
 
-int TempCtrl () 
+//Proyecto más próximo. Hasta ahora, Usa datos actuales y entrega los valores de temperatura
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int ImpTemp();
+
+int main()
 {
-  struct 
-  {
-  char SetTempInicio[4], TempZona[4];
-  
-  } Struct_Temps;
-  
+    int i, Inicio_Temp[4], Temp[4];
+    
+    printf ("   MENU DE OPCIONES\n\n1)Cargar datos\n2)Usar datos actuales\n");
+    scanf ("%d", &i);
+    
+    switch (i) 
+    {
+        case 1:
+        i=0;
+        ImpTemp();
+        break;
+        
+        case 2: 
+        i=0;
+        ImpTemp();
+        break;
+    }
+    
+    i=0;
+    do
+    {
+        printf ("Temperatura seteada para zona %d = %d\n", i+1, Inicio_Temp[i]);
+        i++;
+    }while(i<4);
+    
+    scanf("%d", &i);
+    return 0;
+}
+
+int ImpTemp()
+{
+    int i, Temp[4];
+    i=0;
+    do 
+    {
+        printf ("Temp. zona %d: %d\n", i+1, Temp[i]);
+        i++;
+            
+    }while (i<4);
+    return Temp;
 }
